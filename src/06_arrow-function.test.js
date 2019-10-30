7941cbac0802f25ca17ce278747cda507985024b
MonaMarchetti <marchetti.mona@gmail.com>
2019-10-30 14:49:44 +0100

    06_arrow-function.test

--- src/06_arrow-function.test.js
+++ src/06_arrow-function.test.js
@@ -1,23 +1,20 @@
 // 06: arrow functions
 
-describe("06 Arrow functions", () => {
-  it("Should bind the context to this", () => {
+describe('06 Arrow functions', () => {
+  it('Should bind the context to this', () => {
     const person = {
       age: 1,
 
       add: function(age) {
         // finish the implementation of the next function using an arrow function
         // so we can add some years to the current age and return the new age
-        function f(years) {
-          return this.age + years;
-        }
+        const f = years => age + years
+        return f(age)
+      },
+    }
 
-        return f(age);
-      }
-    };
+    let actual = person.add(1)
 
-    let actual = person.add(1);
-
-    expect(actual).toEqual(2);
-  });
-});
+    expect(actual).toEqual(2)
+  })
+})
